56b93e152b3d593b48cd7329f8494e93d8ddf2cf
Lars Vierbergen <vierbergenlars@gmail.com>
2013-03-08 18:07:01 +0100

    Add node GUI for the debugger

---
+++ gui/index.js
@@ -0,0 +1,62 @@
+var appjs = module.exports= require('appjs');
+var net = require('net');
+
+appjs.serveFilesFrom(__dirname + "/public");
+
+var window = appjs.createWindow({
+	width: 640,
+	height: 460,
+	alpha: false
+});
+
+window.on('create', function() {
+	window.frame.show();
+});
+
+
+
+window.on('ready', function() {
+	window.require = require;
+	window.process = process;
+	window.module = module;
+	window.socket = socket = net.createConnection(process.argv[2]);
+	function setPin(pin, to) {
+		console.log('setPin: '+pin+' to '+to);
+		if(to == "1")
+			window.document.getElementById("led"+pin).checked = true;
+		else
+			window.document.getElementById("led"+pin).checked = false;
+	}
+
+	window.updatePin = function(elem) {
+		if(elem.checked) {
+			socket.write('set '+elem.id[3]+' 1');
+		}
+		else {
+			socket.write('set '+elem.id[3]+' 0');
+		}
+
+	}
+
+	setInterval(function() {if(window.document.getElementById('run').checked)socket.write('cont')}, 1000);
+
+	socket.on('connect', function() {
+		socket.on('data', function(data) {
+			data = data.toString();
+			pins = data.split("\n");
+			for(i in pins) {
+				if(result = /pin ([1-7]): (0|1)/.exec(pins[i])) {
+					console.log(result);
+					setPin(result[1], result[2]);
+				}
+			}
+		});
+		socket.write('get all');
+	});
+    window.addEventListener('keydown', function(e){
+    // show chrome devtools on f12 or commmand+option+j
+    if (e.keyIdentifier === 'F12' || e.keyCode === 74 && e.metaKey && e.altKey) {
+      window.frame.openDevTools();
+    }
+  });
+});
