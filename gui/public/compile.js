5ce2352d2ce188300d9ecdd4bf8a97cdb6220984
Lars Vierbergen <vierbergenlars@gmail.com>
2013-04-15 13:31:44 +0200

    Revert "Run make clean before running make"
    This reverts commit eb1225092e70dcd6ec385f5d4c73b70cef842555.

--- gui/public/compile.js
+++ gui/public/compile.js
@@ -22,48 +22,27 @@
  */
 function exec_make() {
 	var spawn = require('child_process').spawn;
+	var make = spawn('make', [], {'cwd': __dirname+'/..'});
 	var textarea = document.getElementsByTagName('textarea')[0];
-	var clean = spawn('make', ['clean'], {'cwd': __dirname+'/..' });
-	textarea.value+="$ make clean\n";
-	clean.on('exit', function(code) {
-	    if(code > 0) {
+	make.on('exit', function(code) {
+		if(code > 0) {
 			textarea.value+='[make] exited with errorcode '+code+"\n";
-	    }
-	    else {
-	    	var make = spawn('make', [], {'cwd': __dirname+'/..'});
-	        textarea.value+="$ make\n";
-	        make.on('exit', function(code) {
-		        if(code > 0) {
-			        textarea.value+='[make] exited with errorcode '+code+"\n";
-		        }
-		        else {
-			        document.getElementById('enter_debugger').disabled = false;
-			        document.getElementById('enter_debugger').click();
-		        }
-		        document.getElementById('spinner').display="none";
+		}
+		else {
+			document.getElementById('enter_debugger').disabled = false;
+			document.getElementById('enter_debugger').click();
+		}
+		document.getElementById('spinner').display="none";
 
-	        });
-	        
-        	make.stdout.on('data', function(data) {
-		        textarea.value+=data;
-	        });
-	        make.stderr.on('data', function(data) {
-		        textarea.value+='[stderr]'+data;
-	        });
-
-	    }
 	});
+	document.getElementById('spinner').display="block";
+	textarea.value+="$ make\n";
 	
-	clean.stdout.on('data', function(data) {
+	make.stdout.on('data', function(data) {
 		textarea.value+=data;
 	});
-	clean.stderr.on('data', function(data) {
+	make.stderr.on('data', function(data) {
 		textarea.value+='[stderr]'+data;
 	});
-
-
-	document.getElementById('spinner').display="block";
-
-	
 
 }
